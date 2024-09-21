ccb524abd9a03994b4bce505eff02a96c078f302
Piyush <piyushborkar95@gmail.com>
2024-09-21 15:26:51 +0530

    Update RightSide component

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -5,7 +5,7 @@ const RightSide = () => {
     <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
       <a href="mailto:piyushborkar95@gmail.com">
         <p className="text-sm rotate-90 w-72 tracking-widest text-textGreen">
-          reactbd@proton.me
+          mailto@piyush.me
         </p>
       </a>
       <span className="w-[2px] h-32 bg-textDark inline-flex"></span>
